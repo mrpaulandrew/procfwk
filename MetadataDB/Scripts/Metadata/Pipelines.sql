5f7fdab588b265009e5fd97975efecb9da560411
Paul Andrew <paul@mrpaulandrew.com>
2020-03-30 15:10:50 +0100

    Dependency chain support and integrity checks

--- MetadataDB/Scripts/Metadata/Pipelines.sql
+++ MetadataDB/Scripts/Metadata/Pipelines.sql
@@ -3,19 +3,20 @@
 	[DataFactoryId],
 	[StageId],
 	[PipelineName], 
+	[LogicalPredecessorId],
 	[Enabled]
 	) 
 VALUES 
-	(1,1,'Wait 1', 1),
-	(1,1,'Wait 2', 1),
-	(1,1,'Wait 3', 1),
-	(1,1,'Wait 4', 1),
-	(1,2,'Wait 5', 1),
-	(1,2,'Intention Error', 1),
-	(1,2,'Wait 6', 1),
-	(1,2,'Wait 7', 1),
-	(1,3,'Wait 8', 1),
-	(1,3,'Wait 9', 1),
-	(1,4,'Wait 10', 1),
-	(1,4,'Clean Up', 0)
+	(1,1	,'Wait 1'				,NULL		,1),
+	(1,1	,'Wait 2'				,NULL		,1),
+	(1,1	,'Wait 3'				,NULL		,1),
+	(1,1	,'Wait 4'				,NULL		,1),
+	(1,2	,'Wait 5'				,1			,1),
+	(1,2	,'Intention Error'		,2			,1),
+	(1,2	,'Wait 6'				,1			,1),
+	(1,2	,'Wait 7'				,NULL		,1),
+	(1,3	,'Wait 8'				,1			,1),
+	(1,3	,'Wait 9'				,6			,1),
+	(1,4	,'Wait 10'				,9			,1),
+	(1,4	,'Clean Up'				,NULL		,0)
 	;
---
+++ MetadataDB/procfwk/Stored Procedures/CheckStageAndPiplineIntegrity.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [procfwk].[CheckStageAndPiplineIntegrity]
+AS
+
+BEGIN
+
+	SELECT
+		adf.[ResourceGroupName],
+		adf.[DataFactoryName],	
+		base.[StageId],
+		baseStage.[StageName],
+		base.[PipelineId],
+		base.[PipelineName],
+		base.[Enabled],
+		preds.[StageId] 'SuccessorStageId',
+		predsStage.[StageName] AS 'SuccessorStage',
+		preds.[PipelineId] AS 'SuccessorId',
+		preds.[PipelineName] AS 'SuccessorName',
+		CASE
+			WHEN preds.[StageId] > base.[StageId] +1 THEN 'Successor pipeline could be moved to an earlier stage.'
+			WHEN preds.[StageId] = base.[StageId] THEN 'Dependency issue, predeccessor pipeline is currently running in the same stage as successor.'
+			WHEN preds.[PipelineId] IS NOT NULL AND base.[Enabled] = 0 THEN 'Disabled pipeline has downstream successors.'
+			WHEN preds.[PipelineId] IS NOT NULL AND baseStage.[Enabled] = 0 THEN 'Disabled stage has downstream successors.'
+			ELSE NULL
+		END AS 'Information'
+	FROM 
+		--get base pipeline details
+		[procfwk].[Pipelines] base
+		INNER JOIN [procfwk].[DataFactorys] adf
+			ON base.[DataFactoryId] = adf.[DataFactoryId]
+		INNER JOIN [procfwk].[Stages] baseStage
+			ON base.[StageId] = baseStage.[StageId]	
+		--get successor details
+		LEFT OUTER JOIN [procfwk].[Pipelines] preds
+			ON base.[PipelineId] = preds.[LogicalPredecessorId]
+		LEFT OUTER JOIN [procfwk].[Stages] predsStage
+			ON preds.[StageId] = predsStage.[StageId]
+
+END
+
