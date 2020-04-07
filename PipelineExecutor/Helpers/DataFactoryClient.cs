5f5f69317a4263dd00c38167923d123f7d95d4a0
Paul Andrew <paul@mrpaulandrew.com>
2020-04-07 13:40:07 +0100

    Check status function added

---
+++ PipelineExecutor/Helpers/DataFactoryClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.Azure.Management.DataFactory;
+
+namespace PipelineExecutor.Helpers
+{
+    internal static class DataFactoryClient
+    {
+        public static DataFactoryManagementClient createDataFactoryClient(string tenantId, string applicationId, string authenticationKey, string subscriptionId)
+        {
+            var context = new AuthenticationContext("https://login.windows.net/" + tenantId);
+
+            ClientCredential cc = new ClientCredential(applicationId, authenticationKey);
+            AuthenticationResult result = context.AcquireTokenAsync("https://management.azure.com/", cc).Result;
+            ServiceClientCredentials cred = new TokenCredentials(result.AccessToken);
+            
+            var adfClient = new DataFactoryManagementClient(cred)
+            {
+                SubscriptionId = subscriptionId
+            };
+            
+            return adfClient;
+        }
+    }
+}
