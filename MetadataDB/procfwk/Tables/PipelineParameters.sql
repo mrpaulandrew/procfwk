0df155591ec9934b2c2b6b5ca1d8ea51f4512be0
Paul Andrew <paul@mrpaulandrew.com>
2020-06-12 14:47:29 +0100

    Add STRING_ESCAPE and remove param table string limit.

--- MetadataDB/procfwk/Tables/PipelineParameters.sql
+++ MetadataDB/procfwk/Tables/PipelineParameters.sql
@@ -2,7 +2,7 @@
     [ParameterId]    INT           IDENTITY (1, 1) NOT NULL,
     [PipelineId]     INT           NOT NULL,
     [ParameterName]  VARCHAR (128) NOT NULL,
-    [ParameterValue] VARCHAR (128) NULL,
+    [ParameterValue] NVARCHAR(MAX) NULL,
     CONSTRAINT [PK_PipelineParameters] PRIMARY KEY CLUSTERED ([ParameterId] ASC),
     CONSTRAINT [FK_PipelineParameters_Pipelines] FOREIGN KEY ([PipelineId]) REFERENCES [procfwk].[Pipelines] ([PipelineId])
 );
