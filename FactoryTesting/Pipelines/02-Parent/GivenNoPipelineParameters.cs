8d8867ce9032db245ed0380394026267cfe091f3
Paul Andrew <paul@mrpaulandrew.com>
2020-07-02 13:55:02 +0100

    No Worker params test added

---
+++ FactoryTesting/Pipelines/02-Parent/GivenNoPipelineParameters.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System.Threading.Tasks;
+
+namespace FactoryTesting.Pipelines.Parent
+{
+    class GivenNoPipelineParameters
+    {
+        private ParentHelper _helper;
+
+        [OneTimeSetUp]
+        public async Task WhenPipelineRun()
+        {
+            _helper = new ParentHelper()
+                .WithEmptyTable("procfwk.CurrentExecution")
+                .WithEmptyTable("procfwk.PipelineParameters")
+                .WithoutSimulatedError()
+                .WithFailureHandling("Simple"); ;
+            await _helper.RunPipeline();
+        }
+
+        #region Integration tests
+
+        [Test]
+        public void ThenPipelineOutcomeIsSucceeded()
+        {
+            _helper.RunOutcome.Should().Be("Succeeded");
+        }
+
+        #endregion
+
+        #region Functional tests
+
+        #endregion
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _helper.InsertNewPipelineParameters();
+            _helper?.TearDown();
+        }
+    }
+}
