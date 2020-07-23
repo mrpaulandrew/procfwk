a923e1c23c0b5ad3f1435c2a67b1585bfa41a521
Paul Andrew <paul@mrpaulandrew.com>
2020-07-23 13:17:36 +0100

    Testing tear down stored procedures added

---
+++ MetadataDB/procfwkTesting/Stored Procedures/ResetMetadata.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [procfwkTesting].[ResetMetadata]
+AS
+BEGIN	
+	EXEC [procfwkHelpers].[SetDefaultProperties];
+	EXEC [procfwkHelpers].[SetDefaultDataFactorys];
+	EXEC [procfwkHelpers].[SetDefaultStages];
+	EXEC [procfwkHelpers].[SetDefaultPipelines];
+	EXEC [procfwkHelpers].[SetDefaultPipelineParameters];
+	EXEC [procfwkHelpers].[SetDefaultPipelineDependants];
+	EXEC [procfwkHelpers].[SetDefaultRecipients];
+	EXEC [procfwkHelpers].[SetDefaultAlertOutcomes];
+	EXEC [procfwkHelpers].[SetDefaultRecipientPipelineAlerts];
+END;
