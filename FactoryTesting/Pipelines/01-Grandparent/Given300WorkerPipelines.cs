dbb24ec634f2ce3cf57e63cfc0dff75b8bf44f90
Paul Andrew <paul@mrpaulandrew.com>
2020-08-18 13:43:10 +0100

    Add record counts to 300 worker test.

--- FactoryTesting/Pipelines/01-Grandparent/Given300WorkerPipelines.cs
+++ FactoryTesting/Pipelines/01-Grandparent/Given300WorkerPipelines.cs
@@ -24,10 +24,21 @@ namespace FactoryTesting.Pipelines.Grandparent
 
         #region Integration tests
 
-        [Test]
+        [Test, Order(1)]
         public void ThenPipelineOutcomeIsSucceeded()
         {
             _helper.RunOutcome.Should().Be("Succeeded");
+        }
+        [Test, Order(2)]
+        public void ThenCurrentExecutionTableIsEmpty()
+        {
+            _helper.RowCount("procfwk.CurrentExecution").Should().Be(0);
+        }
+
+        [Test, Order(3)]
+        public void Then300ExecutionLogRecords()
+        {
+            _helper.RowCount("procfwk.ExecutionLog").Should().Be(300);
         }
 
         #endregion
