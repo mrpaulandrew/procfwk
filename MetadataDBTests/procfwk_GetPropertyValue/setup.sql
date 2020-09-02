253e10ba9ea7fcd14188bca0bfab08ddf4e33795
Richard Swinbank <richard@firefive.io>
2020-09-02 19:08:50 +0100

    Added property function/proc tests

---
+++ MetadataDBTests/procfwk_GetPropertyValue/setup.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE procfwk_GetPropertyValue.[setup]
+AS
+
+DECLARE @propertyName NVARCHAR(128) = 'TestProperty'
+
+EXEC tSQLt.FakeTable 'procfwk.CurrentProperties'
+
+INSERT INTO procfwk.CurrentProperties (
+  [PropertyName]
+, [PropertyValue]
+) VALUES (
+  @propertyName
+, 'TestPropertyValue'
+)
---
+++ MetadataDBTests/procfwk_GetPropertyValue/test WHEN property exists THEN property value returned.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE procfwk_GetPropertyValue.[test WHEN property exists THEN property value returned]
+AS
+
+-- ARRANGE
+DECLARE @propertyName NVARCHAR(128) = 'TestProperty'
+
+EXEC tSQLt.FakeTable 'procfwk.Properties'
+
+INSERT INTO procfwk.Properties (
+  [PropertyName]
+, [PropertyValue]
+) VALUES (
+  'TestProperty'
+, 'TestPropertyValue'
+)
+
+SELECT N'TestPropertyValue' AS PropertyValue
+INTO #expected
+
+-- ACT
+CREATE TABLE #actual (
+  PropertyValue NVARCHAR(4000)
+)
+
+INSERT INTO #actual (
+  PropertyValue 
+)
+EXEC procfwk.GetPropertyValue @propertyName
+
+-- ASSERT
+EXEC tSQLt.AssertEqualsTable
+  @Expected = '#expected'
+, @Actual = '#actual'
