d399dc2d8e554622da34224b56cc81ad4524b451
Paul Andrew <paul@mrpaulandrew.com>
2020-09-23 15:13:07 +0100

    Demo precursor added

---
+++ MetadataDB/dbo/Stored Procedures/DemoModePrecursor.sql
@@ -0,0 +1,67 @@
+﻿CREATE PROCEDURE [dbo].[DemoModePrecursor]
+AS
+BEGIN
+
+	--lazy..
+	IF ([procfwk].[GetPropertyValueInternal]('ExecutionPrecursorProc')) <> '[dbo].[DemoModePrecursor]'
+	BEGIN
+		EXEC [procfwkHelpers].[AddProperty]
+			@PropertyName = N'ExecutionPrecursorProc',
+			@PropertyValue = N'[dbo].[DemoModePrecursor]';
+	END;
+
+	--reduce wait times
+	;WITH cte AS
+		(
+		SELECT 
+			[PipelineId],
+			LEFT(ABS(CAST(CAST(NEWID() AS VARBINARY(192)) AS INT)),1) AS NewValue
+		FROM 
+			[procfwk].[PipelineParameters]
+		)
+	UPDATE
+		pp
+	SET
+		pp.[ParameterValue] = cte.[NewValue]
+	FROM
+		[procfwk].[PipelineParameters] pp
+		INNER JOIN cte
+			ON pp.[PipelineId] = cte.[PipelineId]
+		INNER JOIN [procfwk].[Pipelines] p
+			ON pp.[PipelineId] = p.[PipelineId]
+	WHERE
+		pp.[ParameterName] LIKE 'Wait%'
+		AND p.[Enabled] = 1;
+
+
+	--for intentional error
+	IF NOT EXISTS
+		(
+		SELECT * FROM [procfwk].[CurrentExecution]
+		)
+		BEGIN
+			UPDATE
+				pp
+			SET
+				pp.[ParameterValue] = 'true'
+			FROM
+				[procfwk].[PipelineParameters] pp
+				INNER JOIN [procfwk].[Pipelines] p
+					ON pp.[PipelineId] = p.[PipelineId]
+			WHERE
+				p.[PipelineName] = 'Intentional Error'
+				AND pp.[ParameterName] = 'RaiseErrors';
+		END;
+
+	--dependency chain failure handling
+	EXEC [procfwkHelpers].[AddProperty]
+		@PropertyName = N'FailureHandling',
+		@PropertyValue = N'DependencyChain';
+
+
+	--short infant iterations
+	EXEC [procfwkHelpers].[AddProperty]
+		@PropertyName = N'PipelineStatusCheckDuration',
+		@PropertyValue = N'5';
+
+END;
