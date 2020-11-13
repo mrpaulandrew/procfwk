ab79af0e0ad88aab1c1f80de2ef083c24882b9f8
Paul Andrew <paul@mrpaulandrew.com>
2020-11-13 15:08:11 +0000

    Validation proc added

---
+++ MetadataDB/procfwk/Stored Procedures/SetLogPipelineValidating.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [procfwk].[SetLogPipelineValidating]
+	(
+	@ExecutionId UNIQUEIDENTIFIER,
+	@StageId INT,
+	@PipelineId INT
+	)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE
+		[procfwk].[CurrentExecution]
+	SET
+		[PipelineStatus] = 'Validating'
+	WHERE
+		[LocalExecutionId] = @ExecutionId
+		AND [StageId] = @StageId
+		AND [PipelineId] = @PipelineId
+END;
