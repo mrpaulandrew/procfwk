904fff49347b5a7931eb4f068539f97c47078c77
Paul Andrew <paul@mrpaulandrew.com>
2020-11-18 12:29:35 +0000

    Update to batch wrapper for overiding restarts.

--- MetadataDB/procfwk/Stored Procedures/BatchWrapper.sql
+++ MetadataDB/procfwk/Stored Procedures/BatchWrapper.sql
@@ -7,6 +7,11 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
+	DECLARE @RestartStatus BIT
+
+	--get restart overide property	
+	SELECT @RestartStatus = [procfwk].[GetPropertyValueInternal]('OverideRestart')
+
 	--check for running batch execution
 	IF EXISTS
 		(
@@ -19,7 +24,8 @@ BEGIN
 				[procfwk].[BatchExecution]
 			WHERE
 				[BatchId] = @BatchId;
-
+			
+			--should never actually be called as handled within Data Factory using the Pipeline Run Check utility
 			RAISERROR('There is already an batch execution run in progress. Stop the related parent pipeline via Data Factory first.',16,1);
 			RETURN 0;
 		END
@@ -27,6 +33,7 @@ BEGIN
 		(
 		SELECT 1 FROM [procfwk].[BatchExecution] WHERE [BatchId] = @BatchId AND ISNULL([BatchStatus],'') = 'Stopped'
 		)
+		AND @RestartStatus = 0
 		BEGIN
 			SELECT
 				@LocalExecutionId = [ExecutionId]
@@ -37,6 +44,43 @@ BEGIN
 				AND ISNULL([BatchStatus],'') = 'Stopped';
 
 			RETURN 0;
+		END
+	ELSE IF EXISTS
+		(
+		SELECT 1 FROM [procfwk].[BatchExecution] WHERE [BatchId] = @BatchId AND ISNULL([BatchStatus],'') = 'Stopped'
+		)
+		AND @RestartStatus = 1
+		BEGIN
+			SET @LocalExecutionId = NEWID();
+			
+			--abandon previous batch execution
+			UPDATE
+				[procfwk].[BatchExecution]
+			SET
+				[BatchStatus] = 'Abandoned'
+			WHERE
+				[BatchId] = @BatchId 
+				AND ISNULL([BatchStatus],'') = 'Stopped';
+
+			--create new batch run record
+			INSERT INTO [procfwk].[BatchExecution]
+				(
+				[BatchId],
+				[ExecutionId],
+				[BatchName],
+				[BatchStatus],
+				[StartDateTime]
+				)
+			SELECT
+				[BatchId],
+				@LocalExecutionId,
+				[BatchName],
+				'Running',
+				GETUTCDATE()
+			FROM
+				[procfwk].[Batches]
+			WHERE
+				[BatchId] = @BatchId;			
 		END
 	ELSE
 		BEGIN
