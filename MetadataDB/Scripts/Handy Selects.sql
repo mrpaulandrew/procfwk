515e7a0ad4f1ce42b64ed79c2ff5d175427cbc8b
Paul Andrew <paul@mrpaulandrew.com>
2020-11-19 10:55:59 +0000

    Hnady selects added.

---
+++ MetadataDB/Scripts/Handy Selects.sql
@@ -0,0 +1,23 @@
+﻿SELECT * FROM [procfwk].[BatchExecution]
+
+SELECT * FROM [procfwk].[CurrentExecution]
+
+SELECT * FROM [procfwkReporting].[CurrentExecutionSummary]
+
+SELECT * FROM [procfwk].[CurrentProperties]
+
+SELECT * FROM [procfwk].[Batches]
+
+SELECT * FROM [procfwk].[BatchStageLink]
+
+SELECT * FROM [procfwk].[Stages]
+
+SELECT * FROM [procfwk].[Pipelines]
+
+--SELECT * FROM [procfwk].[PipelineParameters]
+
+SELECT * FROM [procfwk].[ExecutionLog]
+
+SELECT * FROM [procfwk].[ErrorLog]
+
+--SELECT * FROM [dbo].[ServicePrincipals]
