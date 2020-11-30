64e8683754e9cb782275c83491d4f202a7f1ef34
Paul Andrew <paul@mrpaulandrew.com>
2020-11-30 11:05:35 +0000

    Function project folder organising

--- Functions/Services/PipelineServiceType.cs
+++ Functions/Services/PipelineServiceType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace mrpaulandrew.azure.procfwk
+﻿namespace mrpaulandrew.azure.procfwk
 {
     public enum PipelineServiceType
     {
